5bddd6e46e589936ccf9cd86ef9ade2fa8717ba5
Hananie2019 <60793648+Hananie2019@users.noreply.github.com>
2023-10-05 17:25:59 -0400

    more edits

--- js/main.js
+++ js/main.js
@@ -13,18 +13,16 @@ let turn = 'X';
 let win;
 
 const squares = Array.from(document.querySelectorAll('#board div'));
-
 document.getElementById('board').addEventListener('click', handleTurn);
 const messages = document.querySelector('h2');
 document.getElementById('reset-button').addEventListener('click', init);
 
-
-
-
 function getWinner() {
     let winner = null;
     winningCombos.forEach(function(combo, index) {
-        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        if (board[combo[0]] && board[combo[0]] ===
+             board[combo[1]] && board[combo[0]] === 
+             board[combo[2]]) winner = board[combo[0]];
         });
         return winner ? winner : board.includes('') ? null : 'T';
 };
@@ -54,7 +52,8 @@ function render() {
    
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? 
+    `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 
 init();
